7c16c2fe98ca2d77136160da0d3bb3cf78098bf5
Muhammad-sami-09 <m.sami495233@gmail.com>
2024-09-07 18:55:14 +0500

    Area Of Triangle

---
+++ area of a triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float b;
+	cout<<"enter a value of b : ";
+	cin>>b;
+	float h;
+	cout<<"enter a value of h : ";
+	cin>>h;
+float area = 1.0/2.0*(b*h);
+cout<<"Area of Triangle : "<<area;
+}
